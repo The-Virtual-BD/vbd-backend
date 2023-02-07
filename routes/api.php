75d7725bf9473285cd175e3b25cb7004fb9cbe01
Hussains3 <shussain8978@gmail.com>
2023-02-07 17:49:21 +0600

    Route changing 1.06

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
 
     Route::controller(ProfileController::class)->group(function () {
-        Route::put('/myprofile/pupdate/{user}', 'passwordup');
+        Route::post('/myprofile/pupdate/{user}', 'passwordup');
     });
 
 

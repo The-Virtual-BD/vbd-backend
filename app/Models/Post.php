b4203a1085d756b88f90136f8bf077cffa9312a6
Hussains3 <shussain8978@gmail.com>
2023-02-19 11:58:15 +0600

    Post api 1.01

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+
+
+
+    protected $with = ['author','category'];
+
+
+
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
